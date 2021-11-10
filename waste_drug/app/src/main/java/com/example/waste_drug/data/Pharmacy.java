449307053d4c3f694111da2db5776ad4e69411bd
SeongHo Park <42669772+psh2849@users.noreply.github.com>
2021-11-11 01:15:26 +0900

    Add Data Entity

---
+++ waste_drug/app/src/main/java/com/example/waste_drug/data/Pharmacy.java
@@ -0,0 +1,247 @@
+package com.example.waste_drug.data;
+
+public class Pharmacy {
+    String dutyAddr;
+    String dutyMapimg;
+    String dutyName;
+    String dutyTel1;
+    String dutyTime1c;
+    String dutyTime1s;
+    String dutyTime2c;
+    String dutyTime2s;
+    String dutyTime3c;
+    String dutyTime3s;
+    String dutyTime4c;
+    String dutyTime4s;
+    String dutyTime5c;
+    String dutyTime5s;
+    String dutyTime6c;
+    String dutyTime6s;
+    String dutyTime7c;
+    String dutyTime7s;
+    String dutyTime8c;
+    String dutyTime8s;
+    String hpid;
+    String wgs84Lat;
+    String wgs84Lon;
+    boolean isOpenInNight;
+    boolean isOpenInHoliday;
+    boolean isOpenInSaturday;
+    boolean isOpenInSunday;
+
+    public String getDutyAddr() {
+        return dutyAddr;
+    }
+
+    public void setDutyAddr(String dutyAddr) {
+        this.dutyAddr = dutyAddr;
+    }
+
+    public String getDutyMapimg() {
+        return dutyMapimg;
+    }
+
+    public void setDutyMapimg(String dutyMapimg) {
+        this.dutyMapimg = dutyMapimg;
+    }
+
+    public String getDutyName() {
+        return dutyName;
+    }
+
+    public void setDutyName(String dutyName) {
+        this.dutyName = dutyName;
+    }
+
+    public String getDutyTel1() {
+        return dutyTel1;
+    }
+
+    public void setDutyTel1(String dutyTel1) {
+        this.dutyTel1 = dutyTel1;
+    }
+
+    public String getDutyTime1c() {
+        return dutyTime1c;
+    }
+
+    public void setDutyTime1c(String dutyTime1c) {
+        this.dutyTime1c = dutyTime1c;
+    }
+
+    public String getDutyTime1s() {
+        return dutyTime1s;
+    }
+
+    public void setDutyTime1s(String dutyTime1s) {
+        this.dutyTime1s = dutyTime1s;
+    }
+
+    public String getDutyTime2c() {
+        return dutyTime2c;
+    }
+
+    public void setDutyTime2c(String dutyTime2c) {
+        this.dutyTime2c = dutyTime2c;
+    }
+
+    public String getDutyTime2s() {
+        return dutyTime2s;
+    }
+
+    public void setDutyTime2s(String dutyTime2s) {
+        this.dutyTime2s = dutyTime2s;
+    }
+
+    public String getDutyTime3c() {
+        return dutyTime3c;
+    }
+
+    public void setDutyTime3c(String dutyTime3c) {
+        this.dutyTime3c = dutyTime3c;
+    }
+
+    public String getDutyTime3s() {
+        return dutyTime3s;
+    }
+
+    public void setDutyTime3s(String dutyTime3s) {
+        this.dutyTime3s = dutyTime3s;
+    }
+
+    public String getDutyTime4c() {
+        return dutyTime4c;
+    }
+
+    public void setDutyTime4c(String dutyTime4c) {
+        this.dutyTime4c = dutyTime4c;
+    }
+
+    public String getDutyTime4s() {
+        return dutyTime4s;
+    }
+
+    public void setDutyTime4s(String dutyTime4s) {
+        this.dutyTime4s = dutyTime4s;
+    }
+
+    public String getDutyTime5c() {
+        return dutyTime5c;
+    }
+
+    public void setDutyTime5c(String dutyTime5c) {
+        this.dutyTime5c = dutyTime5c;
+    }
+
+    public String getDutyTime5s() {
+        return dutyTime5s;
+    }
+
+    public void setDutyTime5s(String dutyTime5s) {
+        this.dutyTime5s = dutyTime5s;
+    }
+
+    public String getDutyTime6c() {
+        return dutyTime6c;
+    }
+
+    public void setDutyTime6c(String dutyTime6c) {
+        this.dutyTime6c = dutyTime6c;
+    }
+
+    public String getDutyTime6s() {
+        return dutyTime6s;
+    }
+
+    public void setDutyTime6s(String dutyTime6s) {
+        this.dutyTime6s = dutyTime6s;
+    }
+
+    public String getDutyTime7c() {
+        return dutyTime7c;
+    }
+
+    public void setDutyTime7c(String dutyTime7c) {
+        this.dutyTime7c = dutyTime7c;
+    }
+
+    public String getDutyTime7s() {
+        return dutyTime7s;
+    }
+
+    public void setDutyTime7s(String dutyTime7s) {
+        this.dutyTime7s = dutyTime7s;
+    }
+
+    public String getDutyTime8c() {
+        return dutyTime8c;
+    }
+
+    public void setDutyTime8c(String dutyTime8c) {
+        this.dutyTime8c = dutyTime8c;
+    }
+
+    public String getDutyTime8s() {
+        return dutyTime8s;
+    }
+
+    public void setDutyTime8s(String dutyTime8s) {
+        this.dutyTime8s = dutyTime8s;
+    }
+
+    public String getHpid() {
+        return hpid;
+    }
+
+    public void setHpid(String hpid) {
+        this.hpid = hpid;
+    }
+
+    public String getWgs84Lat() {
+        return wgs84Lat;
+    }
+
+    public void setWgs84Lat(String wgs84Lat) {
+        this.wgs84Lat = wgs84Lat;
+    }
+
+    public String getWgs84Lon() {
+        return wgs84Lon;
+    }
+
+    public void setWgs84Lon(String wgs84Lon) {
+        this.wgs84Lon = wgs84Lon;
+    }
+
+    public boolean isOpenInNight() {
+        return isOpenInNight;
+    }
+
+    public void setOpenInNight(boolean openInNight) {
+        isOpenInNight = openInNight;
+    }
+
+    public boolean isOpenInHoliday() {
+        return isOpenInHoliday;
+    }
+
+    public void setOpenInHoliday(boolean openInHoliday) {
+        isOpenInHoliday = openInHoliday;
+    }
+
+    public boolean isOpenInSaturday() {
+        return isOpenInSaturday;
+    }
+
+    public void setOpenInSaturday(boolean openInSaturday) {
+        isOpenInSaturday = openInSaturday;
+    }
+
+    public boolean isOpenInSunday() {
+        return isOpenInSunday;
+    }
+
+    public void setOpenInSunday(boolean openInSunday) {
+        isOpenInSunday = openInSunday;
+    }
+}
