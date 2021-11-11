143ad6cf269d494a32d2ea5d99a7e1d0cd26cf8e
Sohee <mandariny716@gmail.com>
2021-11-11 16:25:06 +0900

    add marker click event

--- waste_drug/app/src/main/java/com/example/waste_drug/KakaoActivity.java
+++ waste_drug/app/src/main/java/com/example/waste_drug/KakaoActivity.java
@@ -1,7 +1,10 @@
 package com.example.waste_drug;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +12,7 @@ import net.daum.mf.map.api.MapView;
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 
-public class KakaoActivity extends AppCompatActivity {
+public class KakaoActivity extends AppCompatActivity implements MapView.POIItemEventListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,7 @@ public class KakaoActivity extends AppCompatActivity {
         //포인트 좌표의 위도, 경도 설정
         MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.5514579595, 126.951949155);
         mapView.setMapCenterPoint(MARKER_POINT, true);
+        mapView.setPOIItemEventListener(this);
 
         mapViewContainer.addView(mapView);
 
@@ -38,5 +42,28 @@ public class KakaoActivity extends AppCompatActivity {
         marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
 
         mapView.addPOIItem(marker);
+    }
+
+    @Override
+    public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
+        LinearLayout info_view;
+        info_view = findViewById(R.id.info);
+
+        info_view.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) {
+
+    }
+
+    @Override
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+
+    }
+
+    @Override
+    public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
+
     }
 }
