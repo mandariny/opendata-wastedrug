5bdaf3db160d3aab3ba9d639c969b06a6a044e66
Sohee <mandariny716@gmail.com>
2021-11-11 15:32:18 +0900

    add marker in map view

--- waste_drug/app/src/main/java/com/example/waste_drug/KakaoActivity.java
+++ waste_drug/app/src/main/java/com/example/waste_drug/KakaoActivity.java
@@ -6,6 +6,8 @@ import android.view.ViewGroup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import net.daum.mf.map.api.MapView;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 
 public class KakaoActivity extends AppCompatActivity {
 
@@ -17,6 +19,24 @@ public class KakaoActivity extends AppCompatActivity {
         MapView mapView = new MapView(this);
 
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+
+        //포인트 좌표의 위도, 경도 설정
+        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.5514579595, 126.951949155);
+        mapView.setMapCenterPoint(MARKER_POINT, true);
+
         mapViewContainer.addView(mapView);
+
+        //마커 설정
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MARKER_POINT);
+
+        // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+
+        mapView.addPOIItem(marker);
     }
 }
