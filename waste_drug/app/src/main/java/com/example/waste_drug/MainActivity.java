26317665bddee69ec590476085209256628ee9da
Sohee <mandariny716@gmail.com>
2021-11-12 03:45:38 +0900

    set main page

--- waste_drug/app/src/main/java/com/example/waste_drug/MainActivity.java
+++ waste_drug/app/src/main/java/com/example/waste_drug/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.waste_drug;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -10,15 +12,18 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.waste_drug.search.SearchActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 import java.security.MessageDigest;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
-    private void getAppKeyHash() {
+    /*private void getAppKeyHash() {
         try {
             PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
             for (Signature signature : info.signatures) {
@@ -32,57 +37,22 @@ public class MainActivity extends FragmentActivity {
             // TODO Auto-generated catch block
             Log.e("name not found", e.toString());
         }
-    }
+    }*/
 
-
-
-    TabLayout tabs;
-
-    waste_drug_box fragment1;
-    business_time fragment2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //getAppKeyHash();
-        
-        //프래그먼트 생성
-        fragment1 = new waste_drug_box();
-        fragment2 = new business_time();
-
-        getSupportFragmentManager().beginTransaction().add(R.id.container, fragment1).commit();
-
-        //탭바 추가
-        tabs = findViewById(R.id.tabs);
-        tabs.addTab(tabs.newTab().setText("폐의약품 수거함"));
-        tabs.addTab(tabs.newTab().setText("약국 운영 시간"));
-
-        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            //프래그먼트 전환
+        Button button2 = (Button) findViewById((R.id.button2));
+        button2.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                Fragment selected = null;
-                if (position == 0)
-                    selected = fragment1;
-                else if (position == 1)
-                    selected = fragment2;
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
             }
         });
-
     }
-
 }
