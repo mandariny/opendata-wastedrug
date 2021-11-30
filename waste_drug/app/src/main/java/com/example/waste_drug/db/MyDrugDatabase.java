d953117885eb61423385e5748f5a626327955123
SeongHo Park <42669772+psh2849@users.noreply.github.com>
2021-12-01 00:23:09 +0900

    ADD MyDrugInfo Database

--- waste_drug/app/src/main/java/com/example/waste_drug/db/MyDrugDatabase.java
+++ waste_drug/app/src/main/java/com/example/waste_drug/db/MyDrugDatabase.java
@@ -1,2 +1,25 @@
-package com.example.waste_drug.db;public class MyDrugDatabase {
+package com.example.waste_drug.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {MyDrugInfo.class}, version = 1)
+public abstract class MyDrugDatabase extends RoomDatabase {
+    public abstract MyDrugInfoDao myDrugInfoDao();
+
+    private static MyDrugDatabase INSTANCE = null;
+
+    public static MyDrugDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), MyDrugDatabase.class, "mydrug-db").build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
 }
